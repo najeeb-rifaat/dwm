591a98878fec4f50d466ff84791ca7581bc5846d
Najeeb Rifaat <mohamme89d@gmail.com>
2018-08-03 19:35:22 +0800

    fix(lang): use correct xkb command to swich keyboard layout

--- config.h
+++ config.h
@@ -29,8 +29,8 @@ static const Rule rules[] = {
  	 */
  	/* class      instance    title       tags mask     isfloating   monitor */
  	{ "Gimp",     NULL,       NULL,       0,            True,        -1   },
- 	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1   },
-	{ "kaku",     NULL,       NULL,       4 << 4,       0,           -1   },
+ 	{ "firefox",  NULL,       NULL,       1 << 1,       True,        -1   },
+ 	{ "surf",     NULL,       NULL,       1 << 1,       True,        -1   },
 };
 
 /* layout(s) */
@@ -60,11 +60,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *langcmd[]  = { "sh", "-c", "xkb-switch -n"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY|ControlMask,           XK_space,  spawn,          {.v = langcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
