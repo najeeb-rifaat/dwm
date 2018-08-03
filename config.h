0705dc2f95d2953bb09fbe9add664f6fa1fe9eb8
Najeeb Rifaat <mohamme89d@gmail.com>
2018-08-03 19:55:32 +0800

    chore(floating_rules): revert floting browsers

--- config.h
+++ config.h
@@ -29,8 +29,8 @@ static const Rule rules[] = {
  	 */
  	/* class      instance    title       tags mask     isfloating   monitor */
  	{ "Gimp",     NULL,       NULL,       0,            True,        -1   },
- 	{ "Firefox",  NULL,       NULL,       1 << 1,       True,        -1   },
- 	{ "Surf",     NULL,       NULL,       1 << 1,       True,        -1   },
+ 	{ "Firefox",  NULL,       NULL,       1 << 1,       False,       -1   },
+ 	{ "Surf",     NULL,       NULL,       1 << 1,       False,       -1   },
 };
 
 /* layout(s) */
