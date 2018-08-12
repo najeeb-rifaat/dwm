4caf38cf7f5a04c3f87dc9b4d5b34697d034cc52
Najeeb Rifaat <mohamme89d@gmail.com>
2018-08-12 17:47:28 +0800

    chore(background): include background image

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ rm -rf ./pkg ./src ./dwm-**
 makepkg -sif --skipinteg --noconfirm
 sudo install -m644 -D ./sleeplock.service /etc/systemd/system/sleeplock.service
 sudo install -m755 -D ./lock.sh $HOME/.local/bin/lock
+sudo install -m755 -D ./background.png /var/background.png
+sudo install -m755 -D ./background_blur.png /var/background_blur.png
