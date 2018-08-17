a97589544c7e47a38287fb9806d33cc47a0460a3
Najeeb Rifaat <mohamme89d@gmail.com>
2018-08-17 18:00:53 +0700

    chore(status): improve status

--- status.sh
+++ status.sh
@@ -5,11 +5,11 @@
 # This function parses /proc/net/dev file searching for a line containing $interface data.
 # Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
 function get_bytes {
-# Find active network interface
-interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
-line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
-eval $line
-now=$(date +%s%N)
+  # Find active network interface
+  interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
+  line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
+  eval $line
+  now=$(date +%s%N)
 }
 
 # Function which calculates the speed using actual and old byte number.
@@ -17,18 +17,18 @@ now=$(date +%s%N)
 # This function should be called each second.
 
 function get_velocity {
-value=$1
-old_value=$2
-now=$3
+  value=$1
+  old_value=$2
+  now=$3
 
-timediff=$(($now - $old_time))
-velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
-if test "$velKB" -gt 1024
-then
-	echo $(echo "scale=2; $velKB/1024" | bc)MB/s
-else
-	echo ${velKB}KB/s
-fi
+  timediff=$(($now - $old_time))
+  velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
+  if test "$velKB" -gt 1024
+  then
+    echo $(echo "scale=2; $velKB/1024" | bc)MB/s
+  else
+    echo ${velKB}KB/s
+  fi
 }
 
 # Get initial values
@@ -38,59 +38,55 @@ old_transmitted_bytes=$transmitted_bytes
 old_time=$now
 
 print_volume() {
-	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-	if test "$volume" -gt 0
-	then
-		echo -e "\uE05D${volume}"
-	else
-		echo -e "Mute"
-	fi
+  volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
+  if test "$volume" -gt 0
+  then
+    echo -e "${volume}"
+  else
+    echo -e "Mute"
+  fi
 }
 
 print_wifi() {
-	ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
+  ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
 
-	if=wlp2s0
-		while IFS=$': \t' read -r label value
-		do
-			case $label in SSID) SSID=$value
-				;;
-			signal) SIGNAL=$value
-				;;
-		esac
-	done < <(iw "$if" link)
+  if=wlp2s0
+    while IFS=$': \t' read -r label value
+    do
+      case $label in SSID) SSID=$value
+        ;;
+      signal) SIGNAL=$value
+        ;;
+    esac
+  done < <(iw "$if" link)
 
-	echo -e "$SSID $SIGNAL $ip"
+  RESULT=$(echo "${SSID:0:12}" | tr -cd "[:alnum:]")
+  echo "$RESULT $SIGNAL $ip"
 }
 
 print_mem(){
-	memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
-	echo -e "$memfree"
-}
-
-print_mem_cpu_info(){
-	test -f /sys/class/thermal/thermal_zone0/temp || return 0
-  echo $(tmux-mem-cpu-load -a 0 -g 0 -i 1) $(head -c 2 /sys/class/thermal/thermal_zone0/temp)C
+  memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
+  echo -e "$memfree MB"
 }
 
 print_bat(){
-	hash acpi || return 0
-	onl="$(grep "on-line" <(acpi -V))"
-	charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
-	if test -z "$onl"
-	then
-		# suspend when we close the lid
-		#systemctl --user stop inhibit-lid-sleep-on-battery.service
-		echo -e "${charge}"
-	else
-		# On mains! no need to suspend
-		#systemctl --user start inhibit-lid-sleep-on-battery.service
-		echo -e "${charge}"
-	fi
+  hash acpi || return 0
+  onl="$(grep "on-line" <(acpi -V))"
+  charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
+  if test -z "$onl"
+  then
+    # suspend when we close the lid
+    #systemctl --user stop inhibit-lid-sleep-on-battery.service
+    echo -e "${charge}"
+  else
+    # On mains! no need to suspend
+    #systemctl --user start inhibit-lid-sleep-on-battery.service
+    echo -e "${charge}"
+  fi
 }
 
 print_date(){
-	date "+%a %m-%d %H:%M%p"
+  date "+%a %m-%d %H:%M%p"
 }
 
 print_xkb() {
@@ -106,23 +102,28 @@ print_xkb() {
   fi
 }
 
+print_xbacklight() {
+  backight=$(xbacklight | cut -d"." -f1)
+  echo "$backight"
+}
+
 while true
 do
 
-	# Get new transmitted, received byte number values and current time
-	get_bytes
+  # Get new transmitted, received byte number values and current time
+  get_bytes
 
-	# Calculates speeds
-	vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
-	vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
+  # Calculates speeds
+  vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
+  vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-  xsetroot -name "$vel_recv $vel_trans $(print_mem_cpu_info) $(print_wifi) BAT:$(print_bat) VOL:$(print_volume) $(print_date) $(print_xkb)"
+  xsetroot -name "$vel_recv $vel_trans $(print_mem) $(print_wifi) L:$(print_xbacklight) B:$(print_bat) V:$(print_volume) $(print_date) $(print_xkb)"
 
-	# Update old values to perform new calculations
-	old_received_bytes=$received_bytes
-	old_transmitted_bytes=$transmitted_bytes
-	old_time=$now
+  # Update old values to perform new calculations
+  old_received_bytes=$received_bytes
+  old_transmitted_bytes=$transmitted_bytes
+  old_time=$now
 
-	sleep 1
+  sleep 1
 
 done
