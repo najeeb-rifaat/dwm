1577156c64c8dc710234628e174d26a7c7036347
Najeeb R <mohamme89d@gmail.com>
2020-01-01 14:36:08 +0800

    chore(dmenu): update dmenu prompt size

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 12;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Mono:style=Regular:size=14:antialias=true" };
-static const char dmenufont[]       = "SF Mono:style=Regular:size=22:antialias=true";
+static const char *fonts[]          = { "SF Mono:style=Regular:size=16:antialias=true" };
+static const char dmenufont[]       = "SF Mono:style=Regular:size=32:antialias=true";
 
 static const char col_gray1[]       = "#229999";
 static const char col_gray2[]       = "#444444";
