8ebc8a338dddebc33115cb72b3509c4df535c880
Najeeb R <mohamme89d@gmail.com>
2020-02-09 15:19:36 +0800

    fix(border): update border to 5 px and use bigger fonts for DWM in general

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 12;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Mono:style=Regular:size=16:antialias=true" };
+static const char *fonts[]          = { "SF Mono:style=Regular:size=18" };
 static const char dmenufont[]       = "SF Mono:style=Regular:size=32:antialias=true";
 
 static const char col_gray1[]       = "#229999";
